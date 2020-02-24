49cdd36a510275fcdbb10fbbf91217a512a3f81e
Paolo Greppi <grep96@icloud.com>
2020-02-24 16:33:46 +0100

    3.0 (modal with details for every movie displayed [title, year and poster], other improvements)

--- js/model.js
+++ js/model.js
@@ -1,24 +1,24 @@
 const btn_ricerca = document.getElementById('btn_ricerca');
-const btn_delete = document.getElementById('btn_delete');
+//const btn_delete = document.getElementById('btn_delete');
 const container = document.getElementById('card-container');
+const pageTitle = document.getElementById('pageTitle');
 
 btn_ricerca.onclick = function(){
     const p = 1;
     const query = document.getElementById("search").value;
     searchAPI(query, p);
 
-    for(i=2; i<10; i++){
-        if (container.hasChildNodes()) {
-            console.log("Prova");
+    if (container.hasChildNodes()) {
+        for(i=2; i<10; i++){
             const p = i;
-            searchAPI(query, p);
+            searchAPI(query, p)
         }
     }
 
-    btn_delete.style.cssText = 'display: inline-block;';
+    //btn_delete.style.cssText = 'display: inline-block;';
 }
 
-btn_delete.onclick = function(){
+/*btn_delete.onclick = function(){
     btn_delete.style.cssText = 'display: none;';
     document.getElementById('search').value = "";
     document.getElementById('search').select();
@@ -26,7 +26,10 @@ btn_delete.onclick = function(){
     while(container.hasChildNodes()) {
         container.removeChild(container.lastChild);
     }
-}
+
+    //location.reload();
+    console.log(container);
+}*/
 
 class Movie{
     constructor(title, year, imdbID, type, poster){
@@ -112,6 +115,8 @@ function getMovies(aDisplayMovies){
         /*let text= document.createElement('p');
         text.className = 'card-text';*/
         
+        let title = aDisplayMovies[i].title;
+        let year = aDisplayMovies[i].year;
 
         cardContainer.appendChild(card);
         card.appendChild(cardBody);
@@ -121,8 +126,105 @@ function getMovies(aDisplayMovies){
 
         document.getElementById('card-container').replaceWith(cardContainer);
 
-        card.onclick = function(){
-            console.log("Dettagli...");
-        } 
+        //deleteMovies(aDisplayMovies);
+        displayDetails(card, img, title, year);
+    }
+}
+
+/*
+function deleteMovies(aDisplayMovies){
+        pageTitle.onclick = function(){
+        for(let i=0; i<aDisplayMovies.length; i++) {
+            aDisplayMovies.pop();
+        }
+        //location.reload();
+        console.log(aDisplayMovies);
+    }
+    
+}*/
+
+function displayDetails(card, img, title, year){
+    card.onclick = function(){
+        let cardDetails;
+
+        cardDetails = document.getElementById('card-details');
+        cardDetails.style.cssText = 'display: block;';
+        let modalDialog = document.createElement('div');
+        modalDialog.className = 'modal-dialog modal-dialog-centered modal-lg';
+        let modalContent = document.createElement('div');
+        modalContent.className = 'modal-content';
+        modalContent.style.cssText = 'height: 650px;';
+
+        //Body
+        let modalBody = document.createElement('div');
+        modalBody.className = 'modal-body';
+        modalBody.style.cssText = '';
+
+        let modalContainer = document.createElement('div');
+        modalContainer.className = 'row';
+
+        //Close
+        let close = document.createElement('i');
+        close.className = 'fa fa-times-circle';
+        close.style.cssText = 'color: black; cursor: pointer; padding: 1px; position: fixed; margin-top: 42%; margin-left: 26.5%;';
+
+        hideDetails(cardDetails, close);
+
+        //Gestione del poster
+        let modalColImg= document.createElement('div');
+        modalColImg.className = 'col-md-6';
+        let modalContainerImg= document.createElement('div');
+        modalContainerImg.style.cssText = 'margin: 0 auto; height: 100%; width: 80%;';
+        let modalImg = document.createElement('img');
+        modalImg.src = img.src;
+        modalImg.style.cssText = 'height: 450px; width: 300px';
+
+        //Gestione del testo (informazioni del film)
+        let modalColText = document.createElement('div');
+        modalColText.className = 'col-md-6';
+        let modalText = document.createElement('div');
+        let textP = document.createElement('p');
+        textP.style.cssText = 'font-size: 18px; font-weight: bold; text-align: center;';
+        textP.textContent = title + ' [' + year + ']';
+
+        //Gestione della trama
+        let modalContainerTrama = document.createElement('div');
+        modalContainerTrama.style.cssText = 'width: 100%';
+        let modalTitleTrama = document.createElement('p');
+        modalTitleTrama.style.cssText = 'font-size: 16px; font-weight: bold; margin-bottom: 0px; margin-top: 9px; text-align: center;';
+        modalTitleTrama.textContent = 'Trama';
+        let modalTrama = document.createElement('p');
+        modalTrama.style.cssText = 'margin-left: 10px; margin-right: 10px; text-align: justify;';
+        modalTrama.textContent = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni odit et necessitatibus earum vel minima libero, ab inventore tempora repudiandae nihil optio enim ullam minus facere officiis pariatur nesciunt deserunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo neque numquam culpa! Officiis, sunt excepturi! Illo vitae nesciunt ipsam quas! Ipsa modi ab fugiat maxime delectus, deserunt recusandae voluptas eius! Lorem ipsum, dolor sit amet. Lorem ipsum inventore tempora repudiandae nihil optio enim ullam minus facere officiis pariatur.';
+
+        //DOM
+        cardDetails.appendChild(modalDialog);
+        modalDialog.appendChild(modalContent);
+        //modalContent.appendChild(modalHeader);
+        //modalHeader.appendChild(close);
+        modalContent.appendChild(modalBody);
+        modalBody.appendChild(close);
+        modalBody.appendChild(modalContainer);
+
+        modalContainer.appendChild(modalColImg);
+        modalColImg.appendChild(modalContainerImg);
+        modalContainerImg.appendChild(modalImg);
+
+        modalContainer.appendChild(modalColText);
+        modalColText.appendChild(modalText);
+        modalText.appendChild(textP);
+
+        modalContainer.appendChild(modalContainerTrama);
+        modalContainerTrama.appendChild(modalTitleTrama);
+        modalContainerTrama.appendChild(modalTrama);
+
+        document.getElementById('card-details').replaceWith(cardDetails);
+    }
+}
+
+function hideDetails(cardDetails, close){
+    close.onclick = function(){
+        cardDetails.style.cssText = 'display: none;';
+        cardDetails.removeChild(cardDetails.lastChild);
     }
 }
